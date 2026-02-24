d2a72966713edf4e3c252be6d609e6d7f40ef474
dennisanchez444-boop <dennisanchez444@gmail.com>
2026-02-23 20:39:16 -0500

    Ejercicio 1 listo

--- app.js
+++ app.js
@@ -3,18 +3,18 @@
 // Datos (arreglos)
 // =========================
 let vendedores = [
-  { cedula:"1714616123",nombre: "Santiago", apellido: "Mosquera", ventas:10, nivel:""},
-  { cedula:"1708934242",nombre: "Paúl", apellido: "Torres", ventas:12, nivel:""},
- ];
+  { cedula: "1714616123", nombre: "Santiago", apellido: "Mosquera", ventas: 10, nivel: "" },
+  { cedula: "1708934242", nombre: "Paúl", apellido: "Torres", ventas: 12, nivel: "" },
+];
 
 let vendedoresVIP = [
-    { cedula:"0720304056",nombre: "Josselyn", apellido: "Pillajo", ventas:17, nivel:"oro"},
-    { cedula:"1945504089",nombre: "Alexandra", apellido: "Analuisa", ventas:10, nivel:"bronce"},
+  { cedula: "0720304056", nombre: "Josselyn", apellido: "Pillajo", ventas: 17, nivel: "oro" },
+  { cedula: "1945504089", nombre: "Alexandra", apellido: "Analuisa", ventas: 10, nivel: "bronce" },
 
 ];
 
-inicializar=function(){
-  
+inicializar = function () {
+  pintarListaVendedores();
 }
 
 
@@ -29,6 +29,13 @@ function limpiarBusqueda() {
   telefonoBuscarEl.value = "";
 }
 
-
+function pintarListaVendedores() {
+  let html = "<ul class='list'>";
+  for (let i = 0; i < vendedores.length; i++) {
+    html += `<li>${vendedores[i].cedula} | ${vendedores[i].nombre} | ventas: ${vendedores[i].ventas}</li>`;
+  }
+  html += "</ul>";
+  mostrarHtmlEnDiv("contenedorIzquierda", html);
+}
 
 
