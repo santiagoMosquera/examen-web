4e891907656d55da01e8cb5a3d28904dec48feb6
santiagoMosquera <zantycb89@gmail.com>
2026-02-23 19:51:40 -0500

    version inicial

---
+++ app.js
@@ -0,0 +1,33 @@
+// =========================
+// Datos (arreglos)
+// =========================
+let vendedores = [
+  { cedula:"1714616123",nombre: "Santiago", apellido: "Mosquera", ventas:10, nivel:""},
+  { cedula:"1708934242",nombre: "Paúl", apellido: "Torres", ventas:12, nivel:""},
+ ];
+
+let vendedoresVIP = [
+    { cedula:"0720304056",nombre: "Josselyn", apellido: "Pillajo", ventas:17, nivel:"oro"},
+    { cedula:"1945504089",nombre: "Alexandra", apellido: "Analuisa", ventas:10, nivel:"bronce"},
+
+];
+
+inicializar=function(){
+  
+}
+
+
+
+// =========================
+// Búsqueda / movimiento
+// =========================
+function limpiarBusqueda() {
+  indiceEncontrado = -1;
+  btnMover.disabled = true;
+  resultadoBusquedaEl.innerHTML = "<span class='muted'>Sin búsqueda aún</span>";
+  telefonoBuscarEl.value = "";
+}
+
+
+
+
---
+++ utilitarios.js
@@ -0,0 +1,46 @@
+//Muestra el mensaje en el div 
+mostrarTextoEnDiv = function (idComponenteDiv, mensaje) {
+    let componente;
+    componente = document.getElementById(idComponenteDiv);
+    componente.textContent = mensaje;
+}
+
+//Muestra el html en el div
+mostrarHtmlEnDiv = function (idComponenteDiv, html) {
+    let componente;
+    componente = document.getElementById(idComponenteDiv);
+    componente.innerHTML = html;
+}
+//Muestra el mensaje en la caja de texto
+mostrarTextoEnCaja = function (idComponenteCaja, mensaje) {
+    let componente;
+    componente = document.getElementById(idComponenteCaja);
+    componente.value = mensaje;
+}
+//recupera el texto del componente
+recuperarTexto = function (idComponente) {
+    let componente;
+    let valorIngresado;
+    componente = document.getElementById(idComponente);
+    valorIngresado = componente.value;
+    return valorIngresado;
+}
+//recupera el texto del componente como entero
+recuperarEntero = function (idComponente) {
+    let valorCaja = recuperarTexto(idComponente);
+    let valorEntero = parseInt(valorCaja);
+    return valorEntero;
+}
+//habilita el componente
+habilitarComponente = function (idComponente){
+    let componente;
+    componente = document.getElementById(idComponente);
+    componente.disabled=false;
+}
+//deshabilita el componente
+habilitarComponente = function (idComponente){
+    let componente;
+    componente = document.getElementById(idComponente);
+    componente.disabled=true;
+}
+
