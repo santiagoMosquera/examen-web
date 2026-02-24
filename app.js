99f31133181aad010a27fb34b8965c2069b6389d
Misael97001 <chiluisajefferson97@gmail.com>
2026-02-23 21:01:14 -0500

    Ejercicio 4 funcion moverAction

--- app.js
+++ app.js
@@ -2,17 +2,17 @@
 // Datos (arreglos)
 // =========================
 let vendedores = [
-  { cedula:"1714616123",nombre: "Santiago", apellido: "Mosquera", ventas:10, nivel:""},
-  { cedula:"1708934242",nombre: "Paúl", apellido: "Torres", ventas:12, nivel:""},
- ];
+  { cedula: "1714616123", nombre: "Santiago", apellido: "Mosquera", ventas: 10, nivel: "" },
+  { cedula: "1708934242", nombre: "Paúl", apellido: "Torres", ventas: 12, nivel: "" },
+];
 
 let vendedoresVIP = [
-    { cedula:"0720304056",nombre: "Josselyn", apellido: "Pillajo", ventas:17, nivel:"oro"},
-    { cedula:"1945504089",nombre: "Alexandra", apellido: "Analuisa", ventas:10, nivel:"bronce"},
+  { cedula: "0720304056", nombre: "Josselyn", apellido: "Pillajo", ventas: 17, nivel: "oro" },
+  { cedula: "1945504089", nombre: "Alexandra", apellido: "Analuisa", ventas: 10, nivel: "bronce" },
 
 ];
 
-inicializar=function(){
+inicializar = function () {
   pintarListaVendedores();
   pintarListaVendedoresVIP();
 }
@@ -33,66 +33,78 @@ function limpiarBusqueda() {
 
 
 pintarListaVendedores = function () {
-    let contenidoHtml = "<ul class='list'>";
-    for (let i = 0; i < vendedores.length; i++) {
-        let v = vendedores[i];
-        contenidoHtml += "<li>" + v.cedula + " | " + v.nombre + " " + v.apellido + " | ventas: " + v.ventas + "</li>";
-    }
-    contenidoHtml += "</ul>";
-    mostrarHtmlEnDiv("contenedorIzquierda", contenidoHtml);
+  let contenidoHtml = "<ul class='list'>";
+  for (let i = 0; i < vendedores.length; i++) {
+    let v = vendedores[i];
+    contenidoHtml += "<li>" + v.cedula + " | " + v.nombre + " " + v.apellido + " | ventas: " + v.ventas + "</li>";
+  }
+  contenidoHtml += "</ul>";
+  mostrarHtmlEnDiv("contenedorIzquierda", contenidoHtml);
 }
 
 
 
 pintarListaVendedoresVIP = function () {
-    let contenidoHtml = "<ul class='list'>";
-    for (let i = 0; i < vendedoresVIP.length; i++) {
-        let v = vendedoresVIP[i];
-        contenidoHtml += "<li>" + v.nombre + " " + v.apellido + " >> Nivel: " + v.nivel + "</li>";
-    }
-    contenidoHtml += "</ul>";
-    mostrarHtmlEnDiv("contenedorDerecha", contenidoHtml);
+  let contenidoHtml = "<ul class='list'>";
+  for (let i = 0; i < vendedoresVIP.length; i++) {
+    let v = vendedoresVIP[i];
+    contenidoHtml += "<li>" + v.nombre + " " + v.apellido + " >> Nivel: " + v.nivel + "</li>";
+  }
+  contenidoHtml += "</ul>";
+  mostrarHtmlEnDiv("contenedorDerecha", contenidoHtml);
 }
 
 
-agregarVendedor=function(vendedor){
+agregarVendedor = function (vendedor) {
   vendedores.push(vendedor);
-  pintarListaVendedores();  
+  pintarListaVendedores();
 }
 
-agregarVendedorAction=function(){
-  let cedula=recuperarTexto("txtCedula");
-  let nombre=recuperarTexto("txtNombre");
-  let apellido=recuperarTexto("txtApellido");
-  let ventas=recuperarEntero("txtVentas");
-  
-  let vendedor={
+agregarVendedorAction = function () {
+  let cedula = recuperarTexto("txtCedula");
+  let nombre = recuperarTexto("txtNombre");
+  let apellido = recuperarTexto("txtApellido");
+  let ventas = recuperarEntero("txtVentas");
+
+  let vendedor = {
     cedula: cedula, nombre: nombre, apellido: apellido, ventas: ventas, nivel: ""
   };
   agregarVendedor(vendedor);
 }
 
-buscarVendedor=function(cedula){
-  let indiceEncontrado=-1;
-  for (let i=0; i<vendedores.length;i++){
-    if(vendedores[i].cedula==cedula){
-      indiceEncontrado=i;
+buscarVendedor = function (cedula) {
+  let indiceEncontrado = -1;
+  for (let i = 0; i < vendedores.length; i++) {
+    if (vendedores[i].cedula == cedula) {
+      indiceEncontrado = i;
       break;
     }
   }
   return indiceEncontrado;
 }
 
-buscarVendedorAction=function(){
+buscarVendedorAction = function () {
   let cedula = recuperarTexto("txtBuscarCedula");
-  let posicionVendedor=buscarVendedor(cedula);
-  if(posicionVendedor!=-1){
+  let posicionVendedor = buscarVendedor(cedula);
+  if (posicionVendedor != -1) {
     let v = vendedores[posicionVendedor];
-    nombreCompleto= v.nombre + " "+v.apellido;
+    nombreCompleto = v.nombre + " " + v.apellido;
     mostrarHtmlEnDiv("resultadoBusqueda", nombreCompleto);
     habilitarComponente("btnMover");
-  }else{
-    mostrarHtmlEnDiv("resultadoBusqueda","El vendedor con dicha cedula no existe");
+  } else {
+    mostrarHtmlEnDiv("resultadoBusqueda", "El vendedor con dicha cedula no existe");
     deshabilitarComponente("btnMover");
+  }
+}
+
+moverAction = function () {
+  let cedulaBuscar = recuperarTexto("txtBuscarCedula");
+  let indice=buscarVendedor(cedulaBuscar);
+  if(indice!=-1){
+    let vendedorRecuperador=vendedores[indice];
+    vendedoresVIP.push(vendedorRecuperador);
+    vendedores.splice(indice,1);
+    pintarListaVendedores();
+    pintarListaVendedoresVIP();
   }
 }
