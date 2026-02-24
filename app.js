fce3aacb5d691b1d64af9e16d4f5e8a53e2c7268
EDWINCSN <c.edwin32@hotmail.com>
2026-02-23 20:01:32 -0500

    Prueba : se pone el nombre estudiante en comnetario en el archivo app.js

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
+//Nombre: Edwin Geovany Cusin
+
 // =========================
 // Datos (arreglos)
 // =========================
 let vendedores = [
-  { cedula:"1714616123",nombre: "Santiago", apellido: "Mosquera", ventas:10, nivel:""},
-  { cedula:"1708934242",nombre: "Paúl", apellido: "Torres", ventas:12, nivel:""},
- ];
+    { cedula: "1714616123", nombre: "Santiago", apellido: "Mosquera", ventas: 10, nivel: "" },
+    { cedula: "1708934242", nombre: "Paúl", apellido: "Torres", ventas: 12, nivel: "" },
+];
 
 let vendedoresVIP = [
-    { cedula:"0720304056",nombre: "Josselyn", apellido: "Pillajo", ventas:17, nivel:"oro"},
-    { cedula:"1945504089",nombre: "Alexandra", apellido: "Analuisa", ventas:10, nivel:"bronce"},
+    { cedula: "0720304056", nombre: "Josselyn", apellido: "Pillajo", ventas: 17, nivel: "oro" },
+    { cedula: "1945504089", nombre: "Alexandra", apellido: "Analuisa", ventas: 10, nivel: "bronce" },
 
 ];
 
-inicializar=function(){
-  
+inicializar = function() {
+
 }
 
 
@@ -22,12 +24,8 @@ inicializar=function(){
 // Búsqueda / movimiento
 // =========================
 function limpiarBusqueda() {
-  indiceEncontrado = -1;
-  btnMover.disabled = true;
-  resultadoBusquedaEl.innerHTML = "<span class='muted'>Sin búsqueda aún</span>";
-  telefonoBuscarEl.value = "";
+    indiceEncontrado = -1;
+    btnMover.disabled = true;
+    resultadoBusquedaEl.innerHTML = "<span class='muted'>Sin búsqueda aún</span>";
+    telefonoBuscarEl.value = "";
 }
-
-
-
-
