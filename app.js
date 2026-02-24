185c309dbd9ce33eb3cee377263265741e169126
Junior27VQ <favq1999@outlook.com>
2026-02-23 21:10:35 -0500

    funcion pintarVendedoresVIP creada

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ let vendedores = [
 let vendedoresVIP = [
     { cedula:"0720304056",nombre: "Josselyn", apellido: "Pillajo", ventas:17, nivel:"oro"},
     { cedula:"1945504089",nombre: "Alexandra", apellido: "Analuisa", ventas:10, nivel:"bronce"},
-
 ];
 
 inicializar=function(){
   pintarListaVendedores();
+  pintarListaVendedoresVIP();
 }
 
 
@@ -33,10 +33,16 @@ function pintarListaVendedores(){
   //let cmpTabla=document.getElementById("contenedorIzquierda");
   let tabla="<ul>";
   for(let i=0;i<vendedores.length;i++){
-    tabla+="<li>"+vendedores[i].cedula+" | "+vendedores[i].nombre+" "+vendedores[i].apellido+" | ventas: "+vendedores[i].ventas+"<li>";
+    tabla+="<li>"+vendedores[i].cedula+" | "+vendedores[i].nombre+" "+vendedores[i].apellido+" | ventas: "+vendedores[i].ventas+" "+vendedores[i].nivel+"<li>";
   }tabla+="</ul>";
   //cmpTabla.innerHTML=tabla;
   mostrarHtmlEnDiv("contenedorIzquierda",tabla);
 }
-
+function pintarListaVendedoresVIP(){
+  let tabla="<ul>";
+  for(let i=0;i<vendedoresVIP.length;i++){
+    tabla+="<li>"+vendedoresVIP[i].cedula+" | "+vendedoresVIP[i].nombre+" "+vendedoresVIP[i].apellido+" | ventas: "+vendedoresVIP[i].ventas+" "+vendedoresVIP[i].nivel+"<li>";
+  }tabla+="</ul>";
+  mostrarHtmlEnDiv("contenedorDerecha",tabla);
+}
 
