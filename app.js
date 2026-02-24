57dfab5c0f76664d185703d49ecfed3858136cf1
2Erik <erik19yogo@gmail.com>
2026-02-23 20:02:12 -0500

    Comentario nombre

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
+//Alumno: ERIK RODRIGO TOAPANTA VELASCO
+
 // =========================
 // Datos (arreglos)
 // =========================
 let vendedores = [
   { cedula:"1714616123",nombre: "Santiago", apellido: "Mosquera", ventas:10, nivel:""},
   { cedula:"1708934242",nombre: "Paúl", apellido: "Torres", ventas:12, nivel:""},
- ];
+  ];
 
 let vendedoresVIP = [
     { cedula:"0720304056",nombre: "Josselyn", apellido: "Pillajo", ventas:17, nivel:"oro"},
     { cedula:"1945504089",nombre: "Alexandra", apellido: "Analuisa", ventas:10, nivel:"bronce"},
 
-];
+  ];
 
 inicializar=function(){
   
