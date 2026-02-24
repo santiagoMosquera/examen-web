27b4c31cfe3cf3b1f4d58ac6efdee7ef8a1ab6d5
angel-hub31 <moralesmales1@hotmail.com>
2026-02-23 21:46:27 -0500

    se crea la funcion calcularNivel

--- app.js
+++ app.js
@@ -128,6 +128,24 @@ function moverAction() {
 
 
 
+// ------------
+// ejercicio 5
+//--------------
+function calcularNivel(ventas) {
+    if (ventas >= 10 && ventas <= 12) {
+        return "bronce";
+    } 
+    else if (ventas >= 13 && ventas <= 15) {
+        return "plata";
+    } 
+    else if (ventas > 15) {
+        return "oro";
+    } else {
+        return "sin nivel"; 
+    }
+}
+
+
 
 
 // =========================
