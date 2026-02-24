745f5b08d5f6245c3ec4560aa435d44a1b8d68c2
kevinmacash <kevinmacash@gmail.com>
2026-02-23 21:03:53 -0500

    Se completa ejercicio 2

--- app.js
+++ app.js
@@ -7,31 +7,79 @@
 // =========================
 
 let vendedores = [
-  { cedula:"1714616123",nombre: "Santiago", apellido: "Mosquera", ventas:10, nivel:""},
-  { cedula:"1708934242",nombre:
-     "Paúl", apellido: "Torres", ventas:12, nivel:""},
+  {
+    cedula: "1714616123",
+    nombre: "Santiago",
+    apellido: "Mosquera",
+    ventas: 10,
+    nivel: "",
+  },
+  {
+    cedula: "1708934242",
+    nombre: "Paúl",
+    apellido: "Torres",
+    ventas: 12,
+    nivel: "",
+  },
 ];
 let vendedoresVIP = [
-    { cedula:"0720304056",nombre: "Josselyn", apellido: "Pillajo", ventas:17, nivel:"oro"},
-    { cedula:"1945504089",nombre: "Alexandra", apellido: "Analuisa", ventas:10, nivel:"bronce"},
-
+  {
+    cedula: "0720304056",
+    nombre: "Josselyn",
+    apellido: "Pillajo",
+    ventas: 17,
+    nivel: "oro",
+  },
+  {
+    cedula: "1945504089",
+    nombre: "Alexandra",
+    apellido: "Analuisa",
+    ventas: 10,
+    nivel: "bronce",
+  },
 ];
 
 // Función para pintar la lista de vendedores
 function pintarListaVendedores() {
-    let html = "<ul class='list'>";
-    for (let i = 0; i < vendedores.length; i++) {
-        let vendedor = vendedores[i];
-        html += "<li>" + vendedor.cedula + " | " + vendedor.nombre + " " + vendedor.apellido + " | ventas: " + vendedor.ventas + "</li>";
-    }
-    html += "</ul>";
-    mostrarHtmlEnDiv("contenedorIzquierda", html);
+  let html = "<ul class='list'>";
+  for (let i = 0; i < vendedores.length; i++) {
+    let vendedor = vendedores[i];
+    html +=
+      "<li>" +
+      vendedor.cedula +
+      " | " +
+      vendedor.nombre +
+      " " +
+      vendedor.apellido +
+      " | ventas: " +
+      vendedor.ventas +
+      "</li>";
+  }
+  html += "</ul>";
+  mostrarHtmlEnDiv("contenedorIzquierda", html);
 }
 
-inicializar = function() {
-    pintarListaVendedores();
-}
+inicializar = function () {
+  pintarListaVendedores();
+};
 
+function pintarListaVendedoresVIP() {
+  let html = "<ul class='list'>";
+
+  for (let i = 0; i < vendedoresVIP.length; i++) {
+    let vendedor = vendedoresVIP[i];
+    html +=
+      "<li>" +
+      vendedor.nombre +
+      " " +
+      vendedor.apellido +
+      " >>  nivel: " +
+      vendedor.nivel +
+      "</li>";
+  }
+  html += "</ul>";
+  mostrarHtmlEnDiv("contenedorDerecha", html);
+}
 
 // =========================
 // Búsqueda / movimiento
